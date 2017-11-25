5d9c2fecab1475882669eff1632501c2f7cb6625
Mehdi Kabab <pioupioum@gmail.com>
2017-11-25 12:13:45 +0100

    [zsh] Add 'drupal' filetype for ripgrep

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,7 @@
-# use nocorrect alias to prevent auto correct from "fixing" these
+# Use nocorrect alias to prevent auto correct from "fixing" these
 alias sudo='nocorrect sudo'
 
-# directory movement
+# Directory movement
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ls="ls -F"
@@ -10,6 +10,10 @@ alias l='ls -lah'
 alias ll='ls -lh'
 alias la='ls -lAh'
 
+# Editors
 alias vim=nvim
 alias v=vim
+
+# Tools
+alias rg='rg --type-add "drupal:*.{php,tpl.php,module,inc,install,view,engine,info}"'
 
