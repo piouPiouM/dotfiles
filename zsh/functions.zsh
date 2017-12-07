9a7f36b16631ab7295f1014b6357f213d84c8fde
Mehdi Kabab <pioupioum@gmail.com>
2017-12-07 23:43:07 +0100

    [zsh] Update configuration

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,16 +1,23 @@
 # -------------------------------------------------------------------
+# display the top process in CPU usage
+# -------------------------------------------------------------------
+topprocess() {
+  ps -xarco pid=,%cpu=,command= | head -1 | awk '{if($2!=0.0) print $3 " (" $2 "%)"}'
+}
+
+# -------------------------------------------------------------------
 # Dash functions
 # -------------------------------------------------------------------
 # Open argument in Dash
-function dash() {
+dash() {
   open dash://$*
 }
 
-function dman() {
-  open dash://manpages:$*
+dman() {
+  open dash://man:$*
 }
 
-function dphp() {
+dphp() {
   open dash://php:$*
 }
 
@@ -20,13 +27,29 @@ function dphp() {
 # -------------------------------------------------------------------
 path() {
   echo $PATH | tr ":" "\n" | \
-    awk "{ sub(\"/usr\",   \"$fg_no_bold[green]/usr$reset_color\"); \
-           sub(\"/bin\",   \"$fg_no_bold[blue]/bin$reset_color\"); \
-           sub(\"/opt\",   \"$fg_no_bold[cyan]/opt$reset_color\"); \
-           sub(\"/sbin\",  \"$fg_no_bold[magenta]/sbin$reset_color\"); \
-           sub(\"/local\", \"$fg_no_bold[yellow]/local$reset_color\"); \
+    awk "{ sub(\"/usr\",    \"$fg_no_bold[green]/usr$reset_color\"); \
+           sub(\"/bin\",    \"$fg_no_bold[blue]/bin$reset_color\"); \
+           sub(\"/opt\",    \"$fg_no_bold[cyan]/opt$reset_color\"); \
+           sub(\"/sbin\",   \"$fg_no_bold[magenta]/sbin$reset_color\"); \
+           sub(\"/local\",  \"$fg_no_bold[yellow]/local$reset_color\"); \
            sub(\"/.local\", \"$fg_no_bold[yellow]/.local$reset_color\"); \
-           sub(\"/.rvm\",  \"$fg_no_bold[red]/.rvm$reset_color\"); \
+           sub(\"/.rvm\",   \"$fg_no_bold[red]/.rvm$reset_color\"); \
+           print }"
+}
+
+# -------------------------------------------------------------------
+# display a neatly formatted fpath.
+# From https://github.com/zanshin/dotfiles
+# -------------------------------------------------------------------
+fpath() {
+  print -rl -- $fpath | \
+    awk "{ sub(\"/usr\",    \"$fg_no_bold[green]/usr$reset_color\"); \
+           sub(\"/bin\",    \"$fg_no_bold[blue]/bin$reset_color\"); \
+           sub(\"/opt\",    \"$fg_no_bold[cyan]/opt$reset_color\"); \
+           sub(\"/sbin\",   \"$fg_no_bold[magenta]/sbin$reset_color\"); \
+           sub(\"/local\",  \"$fg_no_bold[yellow]/local$reset_color\"); \
+           sub(\"/.local\", \"$fg_no_bold[yellow]/.local$reset_color\"); \
+           sub(\"/.rvm\",   \"$fg_no_bold[red]/.rvm$reset_color\"); \
            print }"
 }
 
@@ -34,10 +57,85 @@ path() {
 # myIP address.
 # From https://github.com/zanshin/dotfiles
 # -------------------------------------------------------------------
-function myip() {
+myip() {
   ifconfig lo0 | grep 'inet ' | sed -e 's/:/ /' | awk '{print "lo0       : " $2}'
   ifconfig en0 | grep 'inet ' | sed -e 's/:/ /' | awk '{print "en0 (IPv4): " $2 " " $3 " " $4 " " $5 " " $6}'
   ifconfig en0 | grep 'inet6 ' | sed -e 's/ / /' | awk '{print "en0 (IPv6): " $2 " " $3 " " $4 " " $5 " " $6}'
   ifconfig en1 | grep 'inet ' | sed -e 's/:/ /' | awk '{print "en1 (IPv4): " $2 " " $3 " " $4 " " $5 " " $6}'
   ifconfig en1 | grep 'inet6 ' | sed -e 's/ / /' | awk '{print "en1 (IPv6): " $2 " " $3 " " $4 " " $5 " " $6}'
 }
+
+# -------------------------------------------------------------------
+# fe [FUZZY PATTERN] - Open the selected file with the default editor
+# -------------------------------------------------------------------
+fe() {
+  local files
+  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+  echo "$files"
+  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+}
+
+# -------------------------------------------------------------------
+# Modified version where you can press
+#   - CTRL-O to open with `open` command,
+#   - CTRL-E or Enter key to open with the $EDITOR
+# -------------------------------------------------------------------
+fo() {
+  local out file key
+  IFS=$'\n' out=($(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
+  fi
+}
+
+# -------------------------------------------------------------------
+# From https://github.com/junegunn/fzf/wiki/Examples#z
+# -------------------------------------------------------------------
+unalias z 2> /dev/null
+z() {
+  [ $# -gt 0 ] && _z "$*" && return
+  cd "$(_z -l 2>&1 | fzf-tmux --height 25% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
+}
+
+# -------------------------------------------------------------------
+# Install (one or multiple) selected application(s)
+# using "brew search" as source input
+# mnemonic [B]rew [I]nstall [P]lugin
+# -------------------------------------------------------------------
+bip() {
+  local inst=$(brew search | fzf -m)
+
+  if [[ $inst ]]; then
+    for prog in $(echo $inst);
+    do; brew install $prog; done;
+  fi
+}
+
+# -------------------------------------------------------------------
+# Update (one or multiple) selected application(s)
+# mnemonic [B]rew [U]pdate [P]lugin
+# -------------------------------------------------------------------
+bup() {
+  local upd=$(brew leaves | fzf -m)
+
+  if [[ $upd ]]; then
+    for prog in $(echo $upd);
+    do; brew upgrade $prog; done;
+  fi
+}
+
+# -------------------------------------------------------------------
+# Delete (one or multiple) selected application(s)
+# mnemonic [B]rew [C]lean [P]lugin (e.g. uninstall)
+# -------------------------------------------------------------------
+bcp() {
+  local uninst=$(brew leaves | fzf -m)
+
+  if [[ $uninst ]]; then
+    for prog in $(echo $uninst);
+    do; brew uninstall $prog; done;
+  fi
+}
+
--- zsh/setopt.zsh
+++ zsh/setopt.zsh
@@ -35,6 +35,7 @@ setopt always_to_end # When completing from the middle of a word, move the curso
 setopt auto_menu # show completion menu on successive tab press. needs unsetop menu_complete to work
 setopt auto_name_dirs # any parameter that is set to the absolute name of a directory immediately becomes a name for that directory
 setopt complete_in_word # Allow completion from within a word/phrase
+setopt complete_aliases
 setopt hash_list_all # whenever a command completion is attempted, make sure the entire command path is hashed first.
 setopt noshwordsplit # use zsh style word splitting
 
@@ -59,6 +60,7 @@ setopt noflowcontrol # no c-s/c-q output freezing
 
 # === Job Control
 
+setopt auto_resume
 setopt longlistjobs # display PID when suspending processes as well
 setopt notify # report the status of backgrounds jobs immediately
 
