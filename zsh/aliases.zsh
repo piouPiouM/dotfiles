e4c0bec679390d8765f181f64bfadb5bdaf636db
Mehdi Kabab <pioupioum@gmail.com>
2018-02-21 13:36:31 +0100

    refactor(shell): remove unused aliases, relocate imgcat and imgls tools

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -23,16 +23,3 @@ alias view='vim -R'
 alias rg='rg --type-add "drupal:*.{php,tpl.php,module,inc,install,view,engine,info}"'
 alias mux='tmuxinator'
 
-# iTerm2
-alias imgcat=~/.iterm2/imgcat
-alias imgls=~/.iterm2/imgls
-alias it2attention=~/.iterm2/it2attention
-alias it2check=~/.iterm2/it2check
-alias it2copy=~/.iterm2/it2copy
-alias it2dl=~/.iterm2/it2dl
-alias it2getvar=~/.iterm2/it2getvar
-alias it2setcolor=~/.iterm2/it2setcolor
-alias it2setkeylabel=~/.iterm2/it2setkeylabel
-alias it2ul=~/.iterm2/it2ul
-alias it2universion=~/.iterm2/it2universion
-
