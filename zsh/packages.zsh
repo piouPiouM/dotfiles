1b778c15a8d42a2ce758fe4c074a58e83ff2c202
Mehdi Kabab <pioupioum@gmail.com>
2018-08-18 13:09:27 +0200

    feat(shell): remove unsued zsh plugins

--- zsh/packages.zsh
+++ zsh/packages.zsh
@@ -1,9 +1,8 @@
 zplug "zplug/zplug", hook-build:"zplug --self-manage"
 zplug "mafredri/zsh-async"
-zplug "sindresorhus/pure", use:pure.zsh, as:theme
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
-zplug "Tarrasch/zsh-bd"
-zplug "ael-code/zsh-colored-man-pages"
-zplug "andrewferrier/fzf-z"
-zplug "urbainvaes/fzf-marks"
-zplug "changyuheng/zsh-interactive-cd"
+zplug "sindresorhus/pure", use:pure.zsh, as:theme         # deps: zsh-async
+zplug "zsh-users/zsh-syntax-highlighting", defer:2        # commandline syntax highlighting
+zplug "Tarrasch/zsh-bd"                                   # go back to a parent directory
+zplug "ael-code/zsh-colored-man-pages"                    # colored man pages
+zplug "changyuheng/zsh-interactive-cd"                    # fzf + fish like cd tab completion
+zplug "zsh-users/zsh-autosuggestions"                     # suggest from history
