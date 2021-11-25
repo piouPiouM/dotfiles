4a0c8fdbb8a8d197d825de7b5e6aa1293cebc514
Mehdi Kabab <pioupioum@gmail.com>
2021-11-25 10:13:36 +0100

    feat(nvim/lsp): add support for Lua

--- vim/after/plugin/lspconfig.lua
+++ vim/after/plugin/lspconfig.lua
@@ -9,11 +9,6 @@ local on_attach = function(client, bufnr)
 
   local opts = { noremap=true, silent=true }
 
-  -- Avoid some errors
-  if client.name == "vimls" then
-     client.resolved_capabilities.code_action = false
-  end
-
   -- Assumes that formatting is handled by eslint.
   if client.name == 'tsserver' then
     client.resolved_capabilities.document_formatting = false
@@ -97,3 +92,37 @@ for _, lsp in ipairs(servers) do
 end
 
 require'trouble'.setup {}
+
+-- lua-language-server
+local sumneko_binary_path = vim.fn.exepath('lua-language-server')
+local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+nvim_lsp.sumneko_lua.setup {
+  cmd = { sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua" };
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
