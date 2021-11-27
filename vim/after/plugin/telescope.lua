fa7bd844333d5ebcf0c48cc8e8f49306f2aac4d2
Mehdi Kabab <pioupioum@gmail.com>
2021-11-27 17:05:30 +0100

    feat(nvim/telescope): attempt to add multiple selection support

--- vim/after/plugin/telescope.lua
+++ vim/after/plugin/telescope.lua
@@ -1,5 +1,25 @@
 local actions = require'telescope.actions'
 local action_layout = require'telescope.actions.layout'
+local action_state = require('telescope.actions.state')
+
+-- https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-889122232
+-- TODO: breaks `symbols` builtin command
+local custom_actions = {
+  fzf_multi_select = function (prompt_bufnr)
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    local num_selections = table.getn(picker:get_multi_selection())
+
+    if num_selections > 1 then
+      local picker = action_state.get_current_picker(prompt_bufnr)
+      for _, entry in ipairs(picker:get_multi_selection()) do
+        vim.cmd(string.format("%s %s", ":vsplit!", entry.value))
+      end
+      vim.cmd('stopinsert')
+    else
+      actions.file_edit(prompt_bufnr)
+    end
+  end
+}
 
 local config = {
   bottom_pane = {
@@ -40,12 +60,14 @@ require'telescope'.setup {
     mappings = {
       n = {
         ['<C-p>'] = action_layout.toggle_preview,
-        ['p'] = action_layout.toggle_preview
+        ['p'] = action_layout.toggle_preview,
+        -- ['<cr>'] = custom_actions.fzf_multi_select,
       },
       i = {
         ['<C-p>'] = action_layout.toggle_preview,
         ["<C-Down>"] = actions.cycle_history_next,
         ["<C-Up>"] = actions.cycle_history_prev,
+        -- ['<cr>'] = custom_actions.fzf_multi_select,
       },
     },
   },
