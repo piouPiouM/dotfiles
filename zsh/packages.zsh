e2d4dd46901c4092f39be2cbe26b548d05fd27cc
Mehdi Kabab <pioupioum@gmail.com>
2022-02-26 20:43:50 +0100

    feat(cli/zsh): use Homebrew to install zplug

--- zsh/packages.zsh
+++ zsh/packages.zsh
@@ -1,9 +1,10 @@
-zplug "zplug/zplug", hook-build:"zplug --self-manage"
-zplug "mafredri/zsh-async"
-zplug "sindresorhus/pure", use:pure.zsh, as:theme         # deps: zsh-async
+zplug "mafredri/zsh-async", from:github
+zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme # deps: zsh-async
+
 zplug "zsh-users/zsh-syntax-highlighting", defer:2        # commandline syntax highlighting
 zplug "Tarrasch/zsh-bd"                                   # go back to a parent directory
 zplug "ael-code/zsh-colored-man-pages"                    # colored man pages
 zplug "changyuheng/zsh-interactive-cd"                    # fzf + fish like cd tab completion
 zplug "zsh-users/zsh-autosuggestions"                     # suggest from history
 zplug 'wfxr/forgit'
+
--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,14 +1,6 @@
-export ZPLUG_HOME=$XDG_DATA_HOME/zplug
-export ZPLUG_CACHE_DIR=$XDG_CACHE_HOME/zplug
+export ZPLUG_HOME="$PPM_BREW_PREFIX/opt/zplug"
 export ZPLUG_LOADFILE=$XDG_CONFIG_HOME/zplug/packages.zsh
 
-# Check if zplug is installed
-if [[ ! -d $ZPLUG_HOME || ! -f $ZPLUG_HOME/init.zsh ]]; then
-  echo "zplug is missing. Launch install."
-  git clone https://github.com/zplug/zplug $ZPLUG_HOME
-fi
-
-# Essential
 source $ZPLUG_HOME/init.zsh
 
 # Install plugins if there are plugins that have not been installed
