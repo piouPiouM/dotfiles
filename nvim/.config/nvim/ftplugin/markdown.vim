a4e10be08669d4a7f64dcef125b3e566869da5be
Mehdi Kabab <pioupioum@gmail.com>
2022-07-10 17:05:55 +0200

    feat(nvim): time to fully switch to Neovim
    Migrate to `init.lua`

--- nvim/.config/nvim/ftplugin/markdown.vim
+++ nvim/.config/nvim/ftplugin/markdown.vim
@@ -3,4 +3,5 @@ setlocal tabstop=4 shiftwidth=4 softtabstop=4
 setlocal textwidth=120
 setlocal colorcolumn=+1
 setlocal omnifunc=htmlcomplete#CompleteTags
+setlocal formatoptions+=w
 
---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,54 @@
+require("ppm.globals")
+
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "ù"
+
+-- To avoid to memorize my final choice
+vim.g.snippets = "luasnip"
+
+-- Support embedded lua
+vim.g.vimsyn_embed = "l"
+
+-- Configure providers to make startup faster {{{
+-- See https://neovim.io/doc/user/provider.html
+
+vim.g.node_host_prog = "/usr/local/bin/neovim-node-host"
+
+if vim.fn.has("macunix") then
+  vim.g.python_host_prog = "/usr/local/bin/python2"
+  vim.g.python3_host_prog = "/usr/local/bin/python3"
+else
+  vim.g.python3_host_prog = "/usr/bin/python"
+end
+
+-- }}}
+
+vim.cmd [[
+  source $XDG_CONFIG_HOME/nvim/config/vim-plug.vim
+  syntax enable
+]]
+
+vim.cmd [[
+  runtime! lua/ppm/config/disable-builtin.lua
+  runtime! lua/ppm/config/options.lua
+]]
+
+-- if filereadable(expand("$HOME/.local/vimrc")) then
+--   source $HOME/.local/vimrc
+-- end
+
+-- if filereadable(expand("$XDG_DATA_HOME/nvim/init.vim")) then
+--   source $XDG_DATA_HOME/nvim/init.vim
+-- end
+
+vim.opt.diffopt:append { "algorithm:patience" }
+vim.opt.background = "dark"
+
+require("colorscheme.nord").setup()
+require("colorscheme.nordbuddy").setup()
+require("colorscheme.nightfox").setup()
+require("colorscheme.catppuccin").setup()
+require("colorscheme.catppuccin").set()
