ccdc51ed24f8d002ff6af1c7ea9a98f30ee06f05
Mehdi Kabab <pioupioum@gmail.com>
2023-05-08 18:02:04 +0200

    fix(nvim/plugin/cmp): resolve error with ghost view

--- nvim/.config/nvim/lua/ppm/plugin/cmp/init.lua
+++ nvim/.config/nvim/lua/ppm/plugin/cmp/init.lua
@@ -68,7 +68,7 @@ cmp.setup({
   }),
   sorting = require("ppm.plugin.cmp.sorting"),
   formatting = require("ppm.plugin.cmp.formatting"),
-  experimental = { ghost_text = true },
+  experimental = { ghost_text = { hl_group = 'Comment' } },
   view = {
     entries = { name = 'custom', selection_order = 'near_cursor' }
   },
