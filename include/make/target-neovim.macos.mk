d23f76797216c90f0ff6ac05ff75b59404144de5
Mehdi Kabab <pioupioum@gmail.com>
2023-11-20 17:05:33 +0100

    fix(build/macos): fix some tasks

--- include/make/target-neovim.macos.mk
+++ include/make/target-neovim.macos.mk
@@ -1,9 +1,9 @@
 install-neovim::
-	@brew install -q luajit luajit-openresty luarocks
-	@brew install -q --fetch-HEAD tree-sitter neovim || exit 0
+	@brew upgrade -q luajit luajit-openresty luarocks
+	@brew upgrade -q --fetch-HEAD tree-sitter neovim || exit 0
 	@$(MAKE) postinstall-neovim
 .PHONY: install-neovim
 
 install-neovim-dependencies::
-	@brew install -q lua-language-server
-.PHONY: install-neovim-dependencies:
+	@brew upgrade -q lua-language-server
+.PHONY: install-neovim-dependencies
