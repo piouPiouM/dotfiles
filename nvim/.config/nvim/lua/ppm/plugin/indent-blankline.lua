21699f564550c698468d27c8a0763d9e7298d0b5
Mehdi Kabab <github.wtvqo@tech.pioupioum.fr>
2023-11-30 08:24:51 +0100

    chore(nvim/plugin): update config to v3

--- nvim/.config/nvim/lua/ppm/plugin/indent-blankline.lua
+++ nvim/.config/nvim/lua/ppm/plugin/indent-blankline.lua
@@ -2,10 +2,12 @@ vim.opt.list = true
 -- vim.opt.listchars:append("space:⋅")
 -- vim.opt.listchars:append("eol:↴")
 
-require("indent_blankline").setup {
-  bufname_exclude = { "NERD_tree.*", "startify", "fzf", "vim-plug", "lspsagaoutline" },
-  filetype_exclude = { "markdown", "help", "packer", "Trouble" },
-  use_treesitter = true,
-  show_current_context = true,
-  show_current_context_start = false,
+require("ibl").setup {
+  exclude = {
+    filetypes = { "Trouble" },
+    buftypes = { "NERD_tree.*", "startify", "fzf", "lspsagaoutline" },
+  },
+  scope = {
+    show_start = false,
+  }
 }
