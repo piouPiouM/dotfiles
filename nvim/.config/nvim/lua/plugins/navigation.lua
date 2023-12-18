683108b7fc8fbeb8b5eee4510c52345ebd7d41f2
Mehdi Kabab <github.wtvqo@tech.pioupioum.fr>
2023-12-18 22:22:11 +0100

    chore(nvim/plugin/lazy): update neotree configuration

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -11,15 +11,41 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    cmd = "Neotree",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim",
     },
-    cmd = "Neotree",
-    config = config("neotree"),
     init = config("neotree_setup"),
+    opts = {
+      close_if_last_window = false,
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      enable_diagnostics = true,
+      open_files_do_not_replace_types = {
+        "terminal",
+        "Trouble",
+        "qf",
+        "TelescopePrompt",
+        "lspsagaoutline",
+      },
+      filesystem = {
+        filtered_items = {
+          hide_by_name = { "node_modules" },
+          always_show = { ".config" },
+          never_show = { ".DS_Store" },
+        },
+        follow_current_file = {
+          enabled = true,
+        },
+        use_libuv_file_watcher = true,
+        window = {
+          mappings = { ["h"] = "toggle_hidden" },
+        },
+      },
+    },
   },
 
   {
