29b15ac763ceafc09e9d198a9e1527bd469a11f5
Mehdi Kabab <github.wtvqo@tech.pioupioum.fr>
2023-12-18 22:22:11 +0100

    chore(nvim/plugin/lazy): update neotree configuration

--- nvim/.config/nvim/lua/ppm/plugin/neotree_setup.lua
+++ nvim/.config/nvim/lua/ppm/plugin/neotree_setup.lua
@@ -1,5 +1,7 @@
 local k = require("ppm.keymaps")
 local u = require("ppm.utils")
 
+vim.g.neo_tree_remove_legacy_commands = 1
+
 u.nquickmap(k.search.tree_reveal, [[<cmd>Neotree dir=%:p:h:h reveal_file=%:p<cr>]])
 u.nquickmap(k.search.tree_reveal_cursor, [[<cmd>Neotree reveal_file=<cfile> hidden<cr>]])
