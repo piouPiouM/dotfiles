8513389f262d1344b2d9c992174d56764d43cc42
Mehdi Kabab <github.wtvqo@tech.pioupioum.fr>
2023-12-18 22:27:08 +0100

    chore(nvim/plugin/lazy): update comment configuration

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -11,7 +11,11 @@ return {
   {
     "numToStr/Comment.nvim",
     dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-    config = config("comment"),
+    opts = function()
+      return {
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      }
+    end,
     event = "VeryLazy",
   },
 
