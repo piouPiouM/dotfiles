b37715a255f3070fe4762e29603595a39eeda2cd
Mehdi Kabab <pioupioum@gmail.com>
2024-02-06 08:25:42 +0100

    chore(themes): update Github themes

--- fzf/.local/share/fzf/themes/github-light-high-contrast.sh
+++ fzf/.local/share/fzf/themes/github-light-high-contrast.sh
@@ -1,5 +1,5 @@
 export FZF_THEME="
   --color=fg:#0e1116,bg:#ffffff,hl:#ffffff
-  --color=fg+:#010409,bg+:#dbe8fb,hl+:#702c00
+  --color=fg+:#0e1116,bg+:#dbe8fb,hl+:#702c00
   --color=info:#744500,prompt:#0349b4,pointer:#622cbc
   --color=marker:#055d20,spinner:#0e1116,header:#eef0f2"
