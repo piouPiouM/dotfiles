dc306061eedf3c50db3f0ed34c5991dd4cf43848
Mehdi Kabab <pioupioum@gmail.com>
2024-02-06 08:19:45 +0100

    fix(build/fonts): avoid copy failure when font type is missing

--- include/make/target-fonts.macos.mk
+++ include/make/target-fonts.macos.mk
@@ -4,5 +4,6 @@
 
 postinstall-fonts:
 	@echo "$(PURPLE)• Installing new fonts$(RESET)"
-	@cp $(FONTS_DIR)/*.{otf,ttf} ~/Library/Fonts/
+	@cp $(FONTS_DIR)/*.otf ~/Library/Fonts/ 2>/dev/null || :
+	@cp $(FONTS_DIR)/*.ttf ~/Library/Fonts/ 2>/dev/null || :
 .PHONY: postinstall-fonts
