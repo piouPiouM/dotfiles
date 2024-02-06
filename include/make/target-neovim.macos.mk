0600f0b984bc92638653f9b7212b50036db812f2
Mehdi Kabab <pioupioum@gmail.com>
2024-02-06 08:16:46 +0100

    fix(build/nvim): use brew the right way

--- include/make/target-neovim.macos.mk
+++ include/make/target-neovim.macos.mk
@@ -1,9 +1,21 @@
 install-neovim::
-	@brew upgrade -q luajit luajit-openresty luarocks
-	@brew upgrade -q --fetch-HEAD tree-sitter neovim || exit 0
+	@brew install -q luajit luajit-openresty luarocks
+	@brew install -q --HEAD tree-sitter || exit 0
+	@brew install -q --HEAD neovim || exit 0
 	@$(MAKE) postinstall-neovim
 .PHONY: install-neovim
 
 install-neovim-dependencies::
-	@brew upgrade -q lua-language-server
+	@brew install -q lua-language-server
 .PHONY: install-neovim-dependencies
+
+update-neovim::
+	@brew upgrade -q luajit luajit-openresty luarocks
+	@brew upgrade -q --fetch-HEAD tree-sitter || exit 0
+	@brew upgrade -q --fetch-HEAD neovim || exit 0
+	@$(MAKE) postinstall-neovim
+.PHONY: update-neovim
+
+update-neovim-dependencies::
+	@brew upgrade -q lua-language-server
+.PHONY: update-neovim-dependencies
