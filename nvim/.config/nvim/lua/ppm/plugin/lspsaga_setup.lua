11d96e5cd6f60a99bc848bd58cfeedb89d53d6d2
Mehdi Kabab <pioupioum@gmail.com>
2024-04-26 09:47:34 +0200

    chore(nvim/keymaps): update LSP finder

--- nvim/.config/nvim/lua/ppm/plugin/lspsaga_setup.lua
+++ nvim/.config/nvim/lua/ppm/plugin/lspsaga_setup.lua
@@ -7,6 +7,6 @@ u.nquickmap(k.lsp.definition, [[<cmd>Lspsaga peek_definition<cr>]])
 u.nquickmap(k.lsp.hover_doc, [[<cmd>Lspsaga hover_doc<cr>]]) -- filetype: sagahover
 u.nquickmap(k.lsp.outline, [[<cmd>Lspsaga outline<cr>]])
 u.nquickmap(k.lsp.rename, [[<cmd>Lspsaga rename ++project<cr>]])
-u.nquickmap(k.lsp.search, [[<cmd>Lspsaga finder<cr>]])
+u.nquickmap(k.lsp.search, [[<cmd>Lspsaga finder tyd+ref+imp+def<cr>]])
 u.nquickmap(k.lsp.diagnostic.cursor, [[<cmd>Lspsaga show_cursor_diagnostics<cr>]])
 u.nquickmap(k.lsp.diagnostic.line, [[<cmd>Lspsaga show_line_diagnostics<cr>]])
