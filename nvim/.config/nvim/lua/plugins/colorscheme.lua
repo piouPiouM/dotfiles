6606c2d8e535896fcf7f86c1ab77819e52410269
Mehdi Kabab <github.wtvqo@tech.pioupioum.fr>
2024-05-06 11:20:15 +0200

    chore(cli/themes): add Kanagawa

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -121,4 +121,10 @@ return {
       dim_inactive = true,
     },
   },
+
+  {
+    "rebelot/kanagawa.nvim",
+    build = 'cp $XDG_DATA_HOME/nvim/lazy/kanagawa.nvim/extras/kanagawa*.conf $XDG_CONFIG_HOME/kitty/themes/',
+    lazy = false,
+  },
 }
