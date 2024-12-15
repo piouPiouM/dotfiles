84a00cc1af023fb963712f1bbb5c79f5960c4af8
Mehdi Kabab <pioupioum@gmail.com>
2024-12-15 18:46:31 +0100

    feat(nvim/plugins): replace typescript-tools.nvim w/ ts_ls

---
+++ nvim/.config/nvim/lua/plugins/typescript.lua
@@ -0,0 +1,42 @@
+local F = require("ppm.toolkit.fp.function")
+local A = require("ppm.toolkit.fp.Array")
+local ft = require("ppm.filetype")
+
+return {
+  {
+    "pmizio/typescript-tools.nvim",
+    enabled = false,
+    build = "npm i -g @styled/typescript-styled-plugin typescript-styled-plugin",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    ft = F.pipe(ft.typescript, A.append("markdown.mdx")),
+    opts = {
+      on_attach = function(client, bufnr)
+        local u = require("ppm.utils")
+        require("twoslash-queries").attach(client, bufnr)
+
+        u.buf_map(bufnr, "n", "go", ":TSToolsOrganizeImports<CR>")
+      end,
+
+      expose_as_code_action = "all",
+      jsx_close_tag = {
+        enable = true,
+        filetypes = ft.jsx,
+      },
+
+      tsserver_plugins = {
+        -- for TypeScript v4.9+
+        "@styled/typescript-styled-plugin",
+        -- or for older TypeScript versions
+        -- "typescript-styled-plugin",
+      },
+    },
+  },
+
+  {
+    "marilari88/twoslash-queries.nvim",
+    opts = {
+      multi_line = true,
+    },
+    ft = ft.typescript,
+  },
+}
