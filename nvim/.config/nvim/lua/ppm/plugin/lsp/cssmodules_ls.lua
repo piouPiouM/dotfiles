619cf053e63df7052b8ba1e19a05dc4537e4aa57
Mehdi Kabab <pioupioum@gmail.com>
2025-02-16 16:04:37 +0100

    fix(nvim/lsp): restore `//^?` queries

--- nvim/.config/nvim/lua/ppm/plugin/lsp/cssmodules_ls.lua
+++ nvim/.config/nvim/lua/ppm/plugin/lsp/cssmodules_ls.lua
@@ -5,7 +5,6 @@ M.config = {
   on_attach = function(client, bufnr)
     -- Prevents conflict with TypeScript server.
     client.server_capabilities.definitionProvider = false
-    require("ppm.plugin.lsp.events").on_attach(client, bufnr)
   end,
 }
 
--- nvim/.config/nvim/lua/ppm/plugin/lsp/json.lua
+++ nvim/.config/nvim/lua/ppm/plugin/lsp/json.lua
@@ -2,7 +2,6 @@ local M = {}
 
 M.name = "jsonls"
 M.config = {
-  on_attach = require("ppm.plugin.lsp.events").on_attach,
   settings = {
     json = { schemas = require("schemastore").json.schemas(), validate = { enable = true } },
   },
