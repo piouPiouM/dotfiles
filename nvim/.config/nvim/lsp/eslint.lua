4f3b2cfed64402ba852005bf6ee4ac70186442f1
Mehdi Kabab <pioupioum@gmail.com>
2025-11-11 16:19:26 +0100

    fix(nvim/lsp): eslint config was not read

--- nvim/.config/nvim/lsp/eslint.lua
+++ nvim/.config/nvim/lsp/eslint.lua
@@ -1,23 +1,9 @@
-local api = vim.api
 local plsp = require("ppm.lsp")
 
-api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+return {
+  on_attach = function(client, bufnr)
     if client.name ~= "eslint" then return end
-
-    local bufnr = args.buf
 
     plsp.event.format("LspEslintFixAll", client, bufnr)
   end,
-})
-
--- api.nvim_create_autocmd("LspDetach", {
---   callback = function(args)
---     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
---     if client.name ~= "eslint" then return end
---
---     api.nvim_buf_del_user_command(0, "Format")
---     vim.keymap.del('n', '<Leader>=', { buffer = true, desc = "Format current buffer with LSP" })
---   end,
--- })
+}
