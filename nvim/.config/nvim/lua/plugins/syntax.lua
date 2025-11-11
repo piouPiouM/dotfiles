947ef361cbcfe5408716b4544bf004dc500bd50e
Mehdi Kabab <pioupioum@gmail.com>
2025-11-11 16:29:29 +0100

    chore(nvim/plugins): sway-vim-syntax no longer exists

--- nvim/.config/nvim/lua/plugins/syntax.lua
+++ nvim/.config/nvim/lua/plugins/syntax.lua
@@ -1,7 +1,6 @@
 return {
-  { "wincent/vim-docvim",           ft = "vim" },
-  { "jxnblk/vim-mdx-js",            ft = "markdown.mdx" },
-  { "fladson/vim-kitty",            ft = "kitty" },
-  { "terminalnode/sway-vim-syntax", ft = "swayconfig" },
-  { "bfontaine/Brewfile.vim",       ft = "brewfile" },
+  { "wincent/vim-docvim",     ft = "vim" },
+  { "jxnblk/vim-mdx-js",      ft = "markdown.mdx" },
+  { "fladson/vim-kitty",      ft = "kitty" },
+  { "bfontaine/Brewfile.vim", ft = "brewfile" },
 }
